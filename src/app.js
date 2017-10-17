a77d86b4a9f316a4d5b29ad3d65a93beadc43e09
Alvaro Contreras <alvaro.contreras@base22.com>
2017-10-17 11:40:39 -0500

    Update app.js to indicate the version of the SDK

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import { Class as Carbon } from "carbonldp";
+import {Class as Carbon} from "carbonldp";
 
 console.log( Carbon.version );
 
 let body = document.querySelector( "body" );
 body.innerHTML = "<h1>It Works!</h1>";
-body.innerHTML += `<p>Your Carbon version is: ${Carbon.version}</p>`;
+body.innerHTML += `<p>Your Carbon SDK version is: ${Carbon.version}</p>`;
